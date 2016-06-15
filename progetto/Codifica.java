5abc59a7603e93f21da20ed5dfa113ee35da2fff
Silvia Gioia Florio <floriosg@gmail.com>
2016-06-15 13:51:12 +0200

    Codifica primo commit

---
+++ progetto/Codifica.java
@@ -0,0 +1,8 @@
+public interface Codifica {
+
+	/* ritorna la stringa codificata */
+	public String codifica(String testo);
+
+	/* ritorna la stringa decodificata */
+	public String decodifica(String testo);
+}
