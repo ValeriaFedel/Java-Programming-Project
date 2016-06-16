d60f3aa783ef1c72fc72338d6741077208a43c59
Silvia Gioia Florio <floriosg@gmail.com>
2016-06-16 14:29:57 +0200

    prova gson - provvisorio

--- json_prova/ProvaJson.java
+++ json_prova/ProvaJson.java
@@ -1,4 +1,20 @@
-class ProvaJson {
+import com.google.gson.*;
+import java.io.*;
 
-	
+class ProvaJson {
+		
+	public static void main(String[] args)  {
+		JsonParser parser = new JsonParser();
+		JsonObject json;
+		try {
+			json = (JsonObject)parser.parse(new FileReader("prova.json"));
+		} catch(IOException e) {
+			System.out.println(e);
+			return;
+		}
+
+		String id = ""+json.get("id");
+
+		System.out.println(id);
+	}
 }
