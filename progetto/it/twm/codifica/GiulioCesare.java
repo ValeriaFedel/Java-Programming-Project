b49b8e1b9b8896f6a9c221807d2cf58d6ec6c7b2
Silvia Gioia Florio <floriosg@gmail.com>
2016-06-20 15:57:58 +0200

    javadoc giulio

--- progetto/it/twm/codifica/GiulioCesare.java
+++ progetto/it/twm/codifica/GiulioCesare.java
@@ -1,16 +1,37 @@
 package it.twm.codifica;
 
+/** <code>GiulioCesare</code> &egrave; un'implementazione 
+  * dell'interfaccia <code>Codifica</code> che permette una
+  * codifica in base al Cifrario di Giulio Cesare.
+  * 
+  * @see Codifica
+  * @author Silvia Florio, Valeria Fedel, Davide Mariuzzi
+  */
 public class GiulioCesare implements Codifica {
+    /* ----------------------------- ATTRIBUTI ----------------------------- */
+    /** Variabile che conterr&agrave; il numero di traslazione all'interno del codice ASCII. */
 	private int shift;
 
+    /* ---------------------------- COSTRUTTORE ---------------------------- */
+    /** Crea un oggetto di tipo GiulioCesare con shift pari a 5.
+    */
 	public GiulioCesare() {
 		this(5);
 	}
 
+	/** Crea un oggetto di tipo GiulioCesare con shift pari a 5.
+	  *@param shift		lo scostamento all'interno dell'ASCII.
+      */
 	public GiulioCesare(int shift) {
 		this.shift = shift;
 	}
 
+	/* ------------------------------ METODI ------------------------------- */
+  	/** Ritorna la stringa codificata.
+      * 
+      * @param testo testo da codificare.
+      * @return stringa con testo codificato.
+      */
 	public String codifica(String testo) {
 		char[] arrayTesto = testo.toCharArray();
 		String nuovoTesto;
@@ -34,6 +55,11 @@ public class GiulioCesare implements Codifica {
 
 	}
 
+	/** Ritorna la stringa decodificata.
+      * 
+      * @param testo testo da decodificare.
+      * @return stringa con testo decodificato.
+      */
 	public String decodifica(String testo) {
 		char[] arrayTesto = testo.toCharArray();
 		String nuovoTesto;
@@ -57,12 +83,17 @@ public class GiulioCesare implements Codifica {
 		return nuovoTesto;
 	}
 
-	private boolean numerico(String str)  {  
+	/** Ritorna <code>true</code> se la stringa &egrave; un numero.
+      * 
+      * @param stirng 		stringa da controllare.
+      * @return boolean se numerico o meno.
+      */
+	private boolean numerico(String string)  {  
 	  try  {  
 
-	    double d = Double.parseDouble(str);  
+	    double d = Double.parseDouble(string);  
 
-	  } catch(NumberFormatException nfe) {  
+	  } catch(NumberFormatException e) {  
 
 	    return false;  
 	  } 
